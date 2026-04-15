0f6bbb7348f80026ddbfad31700431f9b929ee87
C ++ course <arsenijholkin02@gmail.com>
2026-04-15 08:48:14 +0300

    icon

--- open-webui/src/lib/components/chat/Navbar.svelte
+++ open-webui/src/lib/components/chat/Navbar.svelte
@@ -283,7 +283,7 @@
 					{/if}
 					
 					<div class="ml-2 flex items-center pr-2">
-						<img src="/static/splash.png" class="h-6 object-contain" alt="MTS Logo" />
+						<img src="/static/favicon.png" class="size-6 rounded-full" alt="MTS Logo" />
 					</div>
 				</div>
 			</div>
