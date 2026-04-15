e65c92a206c3e9d3e8bf6b87c293802b7a2280f7
C ++ course <arsenijholkin02@gmail.com>
2026-04-15 05:34:39 +0300

    voice

--- services/voice_chat/static/inject.js
+++ services/voice_chat/static/inject.js
@@ -11,9 +11,7 @@
     'use strict';
 
     const protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
-    const voicePort = "VOICE_CHAT_PORT_PLACEHOLDER";
-    const hostName = window.location.hostname;
-    const WS_URL = `${protocol}//${hostName}:${voicePort}/ws/voice`;
+    const WS_URL = `${protocol}//${window.location.host}/ws/voice`;
 
     let overlay = null;
     let ws = null;
